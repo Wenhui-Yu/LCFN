aa8a903060ff58684658df61a94a825191d82346
jianlin.ywh <jianlin.ywh@alibaba-inc.com>
2020-06-05 13:40:53 +0800

    edit

--- model_NGCF.py
+++ model_NGCF.py
@@ -56,7 +56,6 @@ class model_NGCF(object):
             propagations = tf.sparse_tensor_dense_matmul(self.A_hat, embeddings)
             embeddings_1 = propagations + embeddings
             embeddings_2 = tf.multiply(propagations, embeddings)
-            # experiments show that sigmoid performs well
             embeddings = tf.nn.sigmoid(tf.matmul(embeddings_1, self.filters_1[k]) + tf.matmul(embeddings_2, self.filters_2[k]))
             # embeddings = tf.nn.relu(tf.matmul(embeddings_1, self.filters_1[k]) + tf.matmul(embeddings_2, self.filters_2[k]))
             # In the paper, authors choose relu for deep NGCF. In our experiments, we try 1 layer+relu, 1 layer+sigmoid, n layers+relu, and n layers+sigmoid, and find 1 layer+sigmoid is the best choice.
