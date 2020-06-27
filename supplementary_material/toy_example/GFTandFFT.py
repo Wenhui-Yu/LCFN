775b85826923ba3265fb8476fe213d7f1e010184
jianlin.ywh <jianlin.ywh@alibaba-inc.com>
2020-06-27 17:08:50 +0800

    edit

--- supplementary_material/toy_example/GFTandFFT.py
+++ supplementary_material/toy_example/GFTandFFT.py
@@ -2,6 +2,10 @@ import numpy as np
 from scipy.fftpack import fft,ifft
 import matplotlib.pyplot as plt
 from IPython.core.pylabtools import figsize
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
 
 def GFT(x):
     ## graph Fourior transform
@@ -22,9 +26,9 @@ def GFT(x):
 
 ## signals in the time domain
 x1=np.array(range(20))
-y = np.sin(1*2*np.pi/20*x1) ## f = 1
-# y = np.sin(7*2*np.pi/20*x1)  ## f = 7
-# y=np.sin(2*np.pi/20*x1) + np.sin(7*2*np.pi/20*x1)  ## mixed
+# y = np.sin(1*2*np.pi/20*x1) ## f = 1
+y = np.sin(7*2*np.pi/20*x1)  ## f = 7
+# y = np.sin(2*np.pi/20*x1) + np.sin(7*2*np.pi/20*x1)  ## mixed
 
 ## transform
 ft=np.abs(fft(y)) # Fast Fourior tranform
@@ -34,29 +38,35 @@ x2, gft=np.abs(GFT(y)) # graph Fourior transform
 #     print(i)
 
 ## plot and save
-path = 'D:/'
+path = ''
 plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=1, hspace=1)
 plt.subplot(221)
 plt.stem(x1,y,linefmt='black',markerfmt = 'ko', basefmt='grey', use_line_collection=True)
 plt.xlim([-0.5,20.5])
 plt.ylim([-1.8,1.8])
-plt.xlabel(r'Time index $n$')
-plt.ylabel(r'Amplitude')
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.xlabel(r'Time index $n$', fontsize=17)
+plt.ylabel(r'Amplitude', fontsize=17)
 
 plt.subplot(222)
 figsize(12.5, 4)
 plt.stem(x1,abs(ft),linefmt='black',markerfmt = 'ko', basefmt='grey', use_line_collection=True)
 plt.xlim([-0.5,10.5])
 plt.ylim([0,11])
-plt.xlabel(r'Frequency index $k$')
-plt.ylabel(r'Amplitude')
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.xlabel(r'Frequency index $k$', fontsize=17)
+plt.ylabel(r'Amplitude', fontsize=17)
 
 plt.subplot(223)
 plt.stem(x2,gft,linefmt='black',markerfmt = 'ko', basefmt='grey', use_line_collection=True)
 plt.xlim([-0.1,2.1])
 plt.ylim([0,3])
-plt.xlabel(r'Frequency $\lambda_k$')
-plt.ylabel(r'Amplitude')
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.xlabel(r'Frequency $\lambda_k$', fontsize=17)
+plt.ylabel(r'Amplitude', fontsize=17)
 
 plt.savefig(path + 'FFT7.pdf')
 plt.show()
