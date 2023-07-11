c6bf849def3076d5f142a9cbe354d708a91dba78
wangmeng33 <wangmeng33@kuaishou.com>
2023-07-11 23:01:09 +0800

    fix dim bug

--- params/params_LGCN.py
+++ params/params_LGCN.py
@@ -11,7 +11,7 @@ LAMDA_list = {"Amazon": {"CrossEntropy": 0.01, "BPR": 0.01, "WBPR": 0.01, "DLNRS
 LR = LR_list[DATASET][LOSS_FUNCTION]
 LAMDA = LAMDA_list[DATASET][LOSS_FUNCTION]
 OPTIMIZER = ['SGD', 'Adagrad', 'RMSProp', 'Adam'][3]
-FREQUENCY = {'Amazon': 120, 'KuaiRand': 120}[DATASET]
+FREQUENCY = {'Amazon': 128, 'KuaiRand': 128}[DATASET]
 LAYER = 1
 
 all_para = {'GPU_INDEX': GPU_INDEX, 'DATASET': DATASET, 'MODEL': MODEL, 'LR': LR, 'LAMDA': LAMDA, 'EMB_DIM': EMB_DIM,
--- params/params_common.py
+++ params/params_common.py
@@ -5,8 +5,8 @@
 GPU_INDEX = "0"
 DATASET = ['Amazon', 'KuaiRand'][0]
 MODEL = ['MF', 'NCF', 'SCF', 'LightGCN', 'LGCN'][0]
-SAMPLER = ['MF', 'NCF', 'SCF', 'LightGCN', 'LGCN'][0]
-LOSS_FUNCTION = ['CrossEntropy', 'BPR', 'WBPR', 'DLNRS'][0]
+SAMPLER = ['MF', 'NCF', 'SCF', 'LightGCN', 'LGCN'][4]
+LOSS_FUNCTION = ['CrossEntropy', 'BPR', 'WBPR', 'DLNRS'][3]
 EMB_DIM = 128
 BATCH_SIZE = 10000
 TEST_USER_BATCH = {'Amazon': 4096, 'KuaiRand': 4096}[DATASET]
