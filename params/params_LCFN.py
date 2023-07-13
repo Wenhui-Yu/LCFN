26ab87822ccf550b817c7a9554db194e2e1ad597
wangmeng33 <wangmeng33@kuaishou.com>
2023-07-14 03:43:12 +0800

    lcfn

--- params/params_LCFN.py
+++ params/params_LCFN.py
@@ -11,8 +11,8 @@ LAMDA_list = {"Amazon": {"CrossEntropy": 0.01, "BPR": 0.01, "WBPR": 0.01, "DLNRS
 LR = LR_list[DATASET][LOSS_FUNCTION]
 LAMDA = LAMDA_list[DATASET][LOSS_FUNCTION]
 OPTIMIZER = ['SGD', 'Adagrad', 'RMSProp', 'Adam'][3]
-FREQUENCY_USER = {'Amazon': 128, 'KuaiRand': 128}[DATASET]
-FREQUENCY_ITEM = {'Amazon': 128, 'KuaiRand': 128}[DATASET]
+FREQUENCY_USER = {'Amazon': 100, 'KuaiRand': 100}[DATASET]
+FREQUENCY_ITEM = {'Amazon': 50, 'KuaiRand': 50}[DATASET]
 LAYER = 1
 
 all_para = {'GPU_INDEX': GPU_INDEX, 'DATASET': DATASET, 'MODEL': MODEL, 'LR': LR, 'LAMDA': LAMDA, 'EMB_DIM': EMB_DIM,
