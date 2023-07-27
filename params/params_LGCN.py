8ac9691b1033eb01edbd7002c3757676dfb66847
wangmeng33 <wangmeng33@kuaishou.com>
2023-07-27 20:14:26 +0800

    para

--- params/params_LGCN.py
+++ params/params_LGCN.py
@@ -8,7 +8,7 @@ LR_list = {"Amazon": {"CrossEntropy": 0.0005, "BPR": 0.0005, "WBPR": 0.0005, "Sh
            "Movielens": {"CrossEntropy": 0.0005, "BPR": 0.0005, "WBPR": 0.0005, "ShiftMC": 0.002, "DLNRS": 0.0005},
            "KuaiRand": {"CrossEntropy": 0.001, "BPR": 0.0005, "WBPR": 0.0005, "ShiftMC": 0.002, "DLNRS": 0.0005}}
 LAMDA_list = {"Amazon": {"CrossEntropy": 0.1, "BPR": 0.05, "WBPR": 2, "ShiftMC": 0.5, "DLNRS": 0.05},
-              "Movielens": {"CrossEntropy": 0.05, "BPR": 0.02, "WBPR": 10, "ShiftMC": 0.2, "DLNRS": 0.01},
+              "Movielens": {"CrossEntropy": 0.05, "BPR": 0.02, "WBPR": 10, "ShiftMC": 0.1, "DLNRS": 0.01},
               "KuaiRand": {"CrossEntropy": 0.005, "BPR": 0.01, "WBPR": 10, "ShiftMC": 0.2, "DLNRS": 0.0002}}
 LR = LR_list[DATASET][LOSS_FUNCTION]
 LAMDA = LAMDA_list[DATASET][LOSS_FUNCTION]
